b2d4ae7035f5206baf024055a1b9408aef67edad
alifarooq77 <alifarooqmughal77@gmail.com>
2025-08-09 11:20:43 +0500

    Pipeline Demo

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
       <div className="container">
         <span className="navbar-brand d-flex align-items-center">
-          <Coffee className="me-2" /> Nutrition Tracker by Ali
+          <Coffee className="me-2" /> Nutrition Tracker
         </span>
 
         <div className="collapse navbar-collapse" id="navbarNav">
